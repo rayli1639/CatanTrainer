84a25f627879467b8aef7ae7772e9f67fd2d5aaf
rayli1639 <ray.z.li@gmail.com>
2020-11-15 10:38:21 -0500

    Board Masked View

--- app/src/main/java/com/example/catanhelper/BoardView.java
+++ app/src/main/java/com/example/catanhelper/BoardView.java
@@ -1,189 +1,92 @@
 package com.example.catanhelper;
-
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+import android.graphics.Path;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * TODO: document your custom view class.
- */
 public class BoardView extends View {
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-    private Drawable mExampleDrawable;
-
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    private Path hexagonPath;
+    private Path hexagonBorderPath;
+    private float radius;
+    private float width, height;
+    private int maskColor;
 
     public BoardView(Context context) {
         super(context);
-        init(null, 0);
+        init();
     }
 
     public BoardView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init();
     }
 
-    public BoardView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
+    public BoardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.BoardView, defStyle, 0);
-
-        mExampleString = a.getString(
-                R.styleable.BoardView_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.BoardView_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.BoardView_exampleDimension,
-                mExampleDimension);
-
-        if (a.hasValue(R.styleable.BoardView_exampleDrawable)) {
-            mExampleDrawable = a.getDrawable(
-                    R.styleable.BoardView_exampleDrawable);
-            mExampleDrawable.setCallback(this);
-        }
-
-        a.recycle();
-
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
+    private void init() {
+        hexagonPath = new Path();
+        hexagonBorderPath = new Path();
+        maskColor = 0xFF01FF77;
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
+    public void setRadius(float r) {
+        this.radius = r;
+        calculatePath();
+    }
 
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
+    public void setMaskColor(int color) {
+        this.maskColor = color;
+        invalidate();
+    }
+
+    private void calculatePath() {
+        float triangleHeight = (float) (Math.sqrt(3) * radius / 2);
+        float centerX = width / 2;
+        float centerY = height / 2;
+        hexagonPath.moveTo(centerX, centerY + radius);
+        hexagonPath.lineTo(centerX - triangleHeight, centerY + radius / 2);
+        hexagonPath.lineTo(centerX - triangleHeight, centerY - radius / 2);
+        hexagonPath.lineTo(centerX, centerY - radius);
+        hexagonPath.lineTo(centerX + triangleHeight, centerY - radius / 2);
+        hexagonPath.lineTo(centerX + triangleHeight, centerY + radius / 2);
+        hexagonPath.moveTo(centerX, centerY + radius);
+
+        float radiusBorder = radius - 5;
+        float triangleBorderHeight = (float) (Math.sqrt(3) * radiusBorder / 2);
+        hexagonBorderPath.moveTo(centerX, centerY + radiusBorder);
+        hexagonBorderPath.lineTo(centerX - triangleBorderHeight, centerY + radiusBorder / 2);
+        hexagonBorderPath.lineTo(centerX - triangleBorderHeight, centerY - radiusBorder / 2);
+        hexagonBorderPath.lineTo(centerX, centerY - radiusBorder);
+        hexagonBorderPath.lineTo(centerX + triangleBorderHeight, centerY - radiusBorder / 2);
+        hexagonBorderPath.lineTo(centerX + triangleBorderHeight, centerY + radiusBorder / 2);
+        hexagonBorderPath.moveTo(centerX, centerY + radiusBorder);
+        invalidate();
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
-
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
-        }
+    public void onDraw(Canvas c) {
+        super.onDraw(c);
+        c.clipPath(hexagonBorderPath, Region.Op.DIFFERENCE);
+        c.drawColor(Color.WHITE);
+        c.save();
+        c.clipPath(hexagonPath, Region.Op.DIFFERENCE);
+        c.drawColor(maskColor);
+        c.save();
     }
 
-    /**
-     * Gets the example string attribute value.
-     *
-     * @return The example string attribute value.
-     */
-    public String getExampleString() {
-        return mExampleString;
-    }
-
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
-    }
-
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
+    // getting the view size and default radius
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = MeasureSpec.getSize(heightMeasureSpec);
+        radius = height / 2 - 10;
+        calculatePath();
     }
 }

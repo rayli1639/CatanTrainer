19427bc1307cd98318cc28b6c6837fdad62dd4c4
rayli1639 <ray.z.li@gmail.com>
2020-11-14 19:50:34 -0500

    RNG

--- app/src/main/java/com/example/catanhelper/Board.java
+++ app/src/main/java/com/example/catanhelper/Board.java
@@ -16,10 +16,18 @@ public class Board {
     Board() {
         ArrayList<Integer> numbers = new ArrayList<Integer>(
                 Arrays.asList(2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12));
-        int length = numbers.size();
+        HashMap<String, Integer> resources = new HashMap<String, Integer>() {{
+           put("Wheat", WHEAT_COUNT);
+           put("Sheep", SHEEP_COUNT);
+           put("Ore", ORE_COUNT);
+           put("TREE", TREE_COUNT);
+           put("BRICK", BRICK_COUNT);
+        }};
         for (int i = 0; i < 5; i++) {
-            int num = numbers.get((int) (Math.random() * length));
-
+            int length = numbers.size();
+            int rand = numbers.get((int) (Math.random() * length));
+            int num = numbers.get(rand);
+            numbers.remove(new Integer(rand));
         }
     }
 }
